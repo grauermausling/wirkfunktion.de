7dce1bf3b583690d24a58173fe96a56e9121a0d3
grauermausling <grauer.mausling@gmail.com>
2011-05-12 20:22:08 +0000

    Share project "JPAandEnums" into "http://wirkfunktion.de/svn/wirkfunktion"

---
+++ samples/trunk/JPAandEnums/src/de/wirkfunktion/samples/jpa/domain/Person.java
@@ -0,0 +1,98 @@
+/* 
+JPAandEnums
+Copyright (C) 2011  Alexander Schmitt
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package de.wirkfunktion.samples.jpa.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class Person implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private long id;
+  private String name;
+  private Date birthday;
+  private String gender;
+
+  public Person() {
+    super();
+  }
+
+  public Person(final String name, final Date birthday, final Gender gender) {
+    super();
+    this.name = name;
+    this.birthday = birthday;
+    this.gender = gender.toString();
+  }
+
+  public Date getBirthday() {
+    return this.birthday;
+  }
+
+  @Transient
+  public Gender getGender() {
+    return Gender.fromString(getGenderData());
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  public long getId() {
+    return this.id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setBirthday(final Date birthday) {
+    this.birthday = birthday;
+  }
+
+  public void setGender(final Gender gender) {
+    setGenderData(gender.toString());
+  }
+
+  public void setId(final long id) {
+    this.id = id;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "Person [id=" + getId() + ", name=" + getName() + ", birthday=" + getBirthday()
+        + ", gender=" + getGender() + "]";
+  }
+
+  protected String getGenderData() {
+    return this.gender;
+  }
+
+  protected void setGenderData(final String string) {
+    this.gender = string;
+  }
+}
---
+++ samples/trunk/JPAandEnums/test/src/de/wirkfunktion/samples/jpa/TestLauncher.java
@@ -0,0 +1,79 @@
+/* 
+JPAandEnums
+Copyright (C) 2011  Alexander Schmitt
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package de.wirkfunktion.samples.jpa;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import de.wirkfunktion.samples.jpa.domain.Gender;
+import de.wirkfunktion.samples.jpa.domain.Person;
+
+/**
+ * @author alexander
+ *
+ */
+public class TestLauncher {
+
+  public static void main(final String[] args) {
+    new TestLauncher().test();
+  }
+
+  private EntityManagerFactory emf;
+
+  public <T> void createEntity(final T entity) {
+    final EntityManager em = this.emf.createEntityManager();
+    final EntityTransaction tx = em.getTransaction();
+    try {
+      tx.begin();
+      em.persist(entity);
+      tx.commit();
+    } catch (final RuntimeException ex) {
+      if (tx != null && tx.isActive()) tx.rollback();
+      throw ex;
+    } finally {
+      em.close();
+    }
+  }
+
+  public <T> T readEntity(final Class<T> clss, final Object id) {
+    final EntityManager em = this.emf.createEntityManager();
+    try {
+      return em.find(clss, id);
+    } finally {
+      em.close();
+    }
+  }
+
+  void test() {
+    this.emf = Persistence.createEntityManagerFactory("JpaPU");
+    try {
+      final Person willy = new Person("Willy", new Date(), Gender.MALE);
+
+      createEntity(willy);
+      final Object id = willy.getId();
+
+      System.out.println("\nJPA Entity: " + readEntity(Person.class, id));
+    } finally {
+      if (this.emf != null) this.emf.close();
+    }
+  }
+}
