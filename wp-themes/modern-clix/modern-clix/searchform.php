0a4b71e12a9bf9969efc5902e3d6d937a7e39d39
grauer.mausling@gmail.com <grauer.mausling@gmail.com>
2012-08-09 19:43:37 +0200

    removed original theme from release branch

--- wp-themes/modern-clix/modern-clix/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-<p class="search-field"><input type="text" value="<?php the_search_query(); ?>" name="s" id="s" /></p>
-<!-- <input type="submit" id="searchsubmit" value="Search" /> -->
-</form>
