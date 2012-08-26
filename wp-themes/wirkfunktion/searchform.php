bb4546c5941891f66b9bfe288018e22aa687098f
grauer.mausling@gmail.com <grauer.mausling@gmail.com>
2012-08-26 17:38:42 +0200

    Searchform done.

--- wp-themes/wirkfunktion/searchform.php
+++ wp-themes/wirkfunktion/searchform.php
@@ -7,9 +7,6 @@
 
 <form method="get" id="searchform" action="<?php echo home_url(); ?>/">
 	<fieldset>
-		<legend><?php _e('Suche', FB_BASIS_TEXTDOMAIN) ?></legend>
-		<label for="s" class="invisible"><?php _e('Suchbegriffe eingeben', FB_BASIS_TEXTDOMAIN) ?></label>
-		<input type="search" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="<?php _e('Hier suchen', FB_BASIS_TEXTDOMAIN) ?>" />
-		<input type="submit" value="<?php _e('Suche', FB_BASIS_TEXTDOMAIN) ?>" id="searchsubmit" />
+		<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
 	</fieldset>
 </form>
