c620f5b2463a1b8aea7be6411f564bea4bbd9640
grauer.mausling@gmail.com <grauer.mausling@gmail.com>
2012-08-26 11:04:34 +0200

    Solved h6 header list issue to look like a comment

--- wp-themes/wirkfunktion/header.php
+++ wp-themes/wirkfunktion/header.php
@@ -51,7 +51,7 @@
 	<header id="header">
 		<hgroup>
 			
-			<h6><?php bloginfo('description'); ?></h6>
+			<h6><ul><li><?php bloginfo('description'); ?></li></ul></h6>
 			<h1><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>						
 		</hgroup>
 	</header>
