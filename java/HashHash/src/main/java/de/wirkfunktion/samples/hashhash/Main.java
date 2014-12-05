6bbc39f692b1fb25303f17d6fc716b3c804aa3db
Alexander Schmitt <alexander.schmitt@web.de>
2014-12-05 15:43:05 +0100

    Just testing the tools with this intial commit & push.

---
+++ java/HashHash/src/main/java/de/wirkfunktion/samples/hashhash/Main.java
@@ -0,0 +1,10 @@
+package de.wirkfunktion.samples.hashhash;
+
+/**
+ * Created by AlexanderAlSchmitt on 05.12.2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello HashHash!");
+    }
+}
